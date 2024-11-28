c3bc4bad7d779cc9a1fc9e4ed698f2b1ae524035
Grazen0 <josedanielgrayson@proton.me>
2024-11-28 00:51:26 -0500

    feat: bnuuy messages

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::{fs, io, path::Path};
 
 fn bnuuyfy(dir: &Path, bnuuy: &[u8], bnuuys: &mut usize) -> io::Result<()> {
-    if !dir.is_dir() {
-        return Ok(());
-    }
-
-    let _ = fs::write(dir.join("bnuuy.png"), bnuuy);
-    println!("{} bnuuys", bnuuys);
+    let result = fs::write(dir.join("bnuuy.png"), bnuuy);
     *bnuuys += 1;
 
+    match result {
+        Ok(_) => println!("{} bnuuys", bnuuys),
+        Err(_) => eprintln!("bnuuy {} failed", bnuuys),
+    }
+
     for entry in fs::read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
+        let path = entry?.path();
         if path.is_dir() {
             bnuuyfy(&path, bnuuy, bnuuys)?;
         }
@@ -22,7 +21,7 @@ fn bnuuyfy(dir: &Path, bnuuy: &[u8], bnuuys: &mut usize) -> io::Result<()> {
 
 fn main() {
     let bnuuy = include_bytes!("../bnuuy.png");
-    let mut bnuuys = 1;
+    let mut bnuuys = 0;
 
     println!("bnuuy commence");
     bnuuyfy(&Path::new("."), bnuuy, &mut bnuuys).unwrap();
