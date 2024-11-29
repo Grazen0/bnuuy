bc995be797f7317452453bf4e884ed2eeababb8b
Grazen0 <josedanielgrayson@proton.me>
2024-11-28 23:06:54 -0500

    refactor: silent bnuuying

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ use std::{fs, io, path::Path};
 
 fn bnuuyfy(dir: &Path, bnuuy: &[u8], bnuuys: &mut usize) -> io::Result<()> {
     let result = fs::write(dir.join("bnuuy.png"), bnuuy);
-    *bnuuys += 1;
 
-    match result {
-        Ok(_) => println!("{} bnuuys", bnuuys),
-        Err(_) => eprintln!("bnuuy {} failed", bnuuys),
+    if result.is_ok() {
+        *bnuuys += 1;
+        println!("{} bnuuys", bnuuys);
     }
 
     for entry in fs::read_dir(dir)? {
-        let path = entry?.path();
-        if path.is_dir() {
-            bnuuyfy(&path, bnuuy, bnuuys)?;
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if path.is_dir() {
+                let _ = bnuuyfy(&path, bnuuy, bnuuys);
+            }
         }
     }
 
@@ -24,6 +25,6 @@ fn main() {
     let mut bnuuys = 0;
 
     println!("bnuuy commence");
-    bnuuyfy(&Path::new("."), bnuuy, &mut bnuuys).unwrap();
+    let _ = bnuuyfy(&Path::new("."), bnuuy, &mut bnuuys);
     println!("bnuuy completion");
 }
